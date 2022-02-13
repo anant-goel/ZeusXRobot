786e0d6a08d5ed8e297caa26a22cb0bb972cd43c
anant-goel <85389190+anant-goel@users.noreply.github.com>
2022-02-13 06:55:53 +0530

    Update config.py

--- ZeusXRobot/config.py
+++ ZeusXRobot/config.py
@@ -45,7 +45,7 @@ class Config(object):
     #List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list('elevated_users.json', 'tigers')
     WOLVES = get_user_list('elevated_users.json', 'whitelists')
-    DONATION_LINK = https://t.me/PcGamesAllForFree # EG, paypal
+    DONATION_LINK = "https://t.me/PcGamesAllForFree" # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
